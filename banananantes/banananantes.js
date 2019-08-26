094ae96bb6eaf163e01b97a99f43cd6fe4934697
pierkiroule <39243998+pierkiroule@users.noreply.github.com>
2019-08-26 18:59:16 +0200

    Add files via upload

---
+++ banananantes/banananantes.js
@@ -0,0 +1,603 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [
+		{name:"banananantes_atlas_", frames: [[502,3130,500,624],[1004,626,500,624],[502,1878,500,624],[1004,3130,500,624],[502,2504,500,624],[2510,1252,500,624],[1506,1252,500,624],[2008,1252,500,624],[3012,1252,500,624],[3514,1252,500,624],[1506,1878,500,624],[3012,1878,500,624],[1004,2504,500,624],[3514,1878,500,624],[3514,2504,500,624],[2008,3130,500,624],[1506,2504,500,624],[1506,3130,500,624],[2008,1878,500,624],[2510,2504,500,624],[2510,3130,500,624],[2008,2504,500,624],[2510,1878,500,624],[1506,626,500,624],[3012,2504,500,624],[1004,0,500,624],[2510,0,500,624],[502,0,500,624],[0,2504,500,624],[0,626,500,624],[0,3130,500,624],[3514,0,500,624],[0,1878,500,624],[1506,0,500,624],[1004,1878,500,624],[0,0,500,624],[3012,0,500,624],[2008,0,500,624],[0,1252,500,624],[502,1252,500,624],[502,626,500,624],[2008,626,500,624],[2510,626,500,624],[3012,626,500,624],[3514,626,500,624],[1004,1252,500,624]]}
+];
+
+
+// symbols:
+
+
+
+(lib.bananantes_0 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_1 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_10 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(2);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_11 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(3);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_12 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(4);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_13 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(5);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_14 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(6);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_15 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(7);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_16 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(8);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_17 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(9);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_18 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(10);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_19 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(11);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_2 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(12);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_20 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(13);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_21 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(14);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_22 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(15);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_23 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(16);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_24 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(17);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_25 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(18);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_26 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(19);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_27 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(20);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_28 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(21);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_29 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(22);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_3 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(23);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_30 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(24);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_31 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(25);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_32 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(26);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_33 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(27);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_34 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(28);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_35 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(29);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_36 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(30);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_37 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(31);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_38 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(32);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_39 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(33);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_4 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(34);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_40 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(35);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_41 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(36);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_42 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(37);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_43 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(38);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_44 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(39);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_45 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(40);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_5 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(41);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_6 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(42);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_7 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(43);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_8 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(44);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.bananantes_9 = function() {
+	this.initialize(ss["banananantes_atlas_"]);
+	this.gotoAndStop(45);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Symbole1 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Calque_1
+	this.instance = new lib.bananantes_0();
+	this.instance.parent = this;
+
+	this.instance_1 = new lib.bananantes_1();
+	this.instance_1.parent = this;
+
+	this.instance_2 = new lib.bananantes_2();
+	this.instance_2.parent = this;
+
+	this.instance_3 = new lib.bananantes_3();
+	this.instance_3.parent = this;
+
+	this.instance_4 = new lib.bananantes_4();
+	this.instance_4.parent = this;
+
+	this.instance_5 = new lib.bananantes_5();
+	this.instance_5.parent = this;
+
+	this.instance_6 = new lib.bananantes_6();
+	this.instance_6.parent = this;
+
+	this.instance_7 = new lib.bananantes_7();
+	this.instance_7.parent = this;
+
+	this.instance_8 = new lib.bananantes_8();
+	this.instance_8.parent = this;
+
+	this.instance_9 = new lib.bananantes_9();
+	this.instance_9.parent = this;
+
+	this.instance_10 = new lib.bananantes_10();
+	this.instance_10.parent = this;
+
+	this.instance_11 = new lib.bananantes_11();
+	this.instance_11.parent = this;
+
+	this.instance_12 = new lib.bananantes_12();
+	this.instance_12.parent = this;
+
+	this.instance_13 = new lib.bananantes_13();
+	this.instance_13.parent = this;
+
+	this.instance_14 = new lib.bananantes_14();
+	this.instance_14.parent = this;
+
+	this.instance_15 = new lib.bananantes_15();
+	this.instance_15.parent = this;
+
+	this.instance_16 = new lib.bananantes_16();
+	this.instance_16.parent = this;
+
+	this.instance_17 = new lib.bananantes_17();
+	this.instance_17.parent = this;
+
+	this.instance_18 = new lib.bananantes_18();
+	this.instance_18.parent = this;
+
+	this.instance_19 = new lib.bananantes_19();
+	this.instance_19.parent = this;
+
+	this.instance_20 = new lib.bananantes_20();
+	this.instance_20.parent = this;
+
+	this.instance_21 = new lib.bananantes_21();
+	this.instance_21.parent = this;
+
+	this.instance_22 = new lib.bananantes_22();
+	this.instance_22.parent = this;
+
+	this.instance_23 = new lib.bananantes_23();
+	this.instance_23.parent = this;
+
+	this.instance_24 = new lib.bananantes_24();
+	this.instance_24.parent = this;
+
+	this.instance_25 = new lib.bananantes_25();
+	this.instance_25.parent = this;
+
+	this.instance_26 = new lib.bananantes_26();
+	this.instance_26.parent = this;
+
+	this.instance_27 = new lib.bananantes_27();
+	this.instance_27.parent = this;
+
+	this.instance_28 = new lib.bananantes_28();
+	this.instance_28.parent = this;
+
+	this.instance_29 = new lib.bananantes_29();
+	this.instance_29.parent = this;
+
+	this.instance_30 = new lib.bananantes_30();
+	this.instance_30.parent = this;
+
+	this.instance_31 = new lib.bananantes_31();
+	this.instance_31.parent = this;
+
+	this.instance_32 = new lib.bananantes_32();
+	this.instance_32.parent = this;
+
+	this.instance_33 = new lib.bananantes_33();
+	this.instance_33.parent = this;
+
+	this.instance_34 = new lib.bananantes_34();
+	this.instance_34.parent = this;
+
+	this.instance_35 = new lib.bananantes_35();
+	this.instance_35.parent = this;
+
+	this.instance_36 = new lib.bananantes_36();
+	this.instance_36.parent = this;
+
+	this.instance_37 = new lib.bananantes_37();
+	this.instance_37.parent = this;
+
+	this.instance_38 = new lib.bananantes_38();
+	this.instance_38.parent = this;
+
+	this.instance_39 = new lib.bananantes_39();
+	this.instance_39.parent = this;
+
+	this.instance_40 = new lib.bananantes_40();
+	this.instance_40.parent = this;
+
+	this.instance_41 = new lib.bananantes_41();
+	this.instance_41.parent = this;
+
+	this.instance_42 = new lib.bananantes_42();
+	this.instance_42.parent = this;
+
+	this.instance_43 = new lib.bananantes_43();
+	this.instance_43.parent = this;
+
+	this.instance_44 = new lib.bananantes_44();
+	this.instance_44.parent = this;
+
+	this.instance_45 = new lib.bananantes_45();
+	this.instance_45.parent = this;
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance}]}).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_2}]},1).to({state:[{t:this.instance_3}]},1).to({state:[{t:this.instance_4}]},1).to({state:[{t:this.instance_5}]},2).to({state:[{t:this.instance_6}]},1).to({state:[{t:this.instance_7}]},1).to({state:[{t:this.instance_8}]},1).to({state:[{t:this.instance_9}]},1).to({state:[{t:this.instance_10}]},1).to({state:[{t:this.instance_11}]},1).to({state:[{t:this.instance_12}]},1).to({state:[{t:this.instance_13}]},1).to({state:[{t:this.instance_14}]},2).to({state:[{t:this.instance_15}]},1).to({state:[{t:this.instance_16}]},1).to({state:[{t:this.instance_17}]},1).to({state:[{t:this.instance_18}]},1).to({state:[{t:this.instance_19}]},1).to({state:[{t:this.instance_20}]},1).to({state:[{t:this.instance_21}]},1).to({state:[{t:this.instance_22}]},1).to({state:[{t:this.instance_23}]},2).to({state:[{t:this.instance_24}]},15).to({state:[{t:this.instance_25}]},2).to({state:[{t:this.instance_26}]},1).to({state:[{t:this.instance_27}]},1).to({state:[{t:this.instance_28}]},1).to({state:[{t:this.instance_29}]},1).to({state:[{t:this.instance_30}]},1).to({state:[{t:this.instance_31}]},1).to({state:[{t:this.instance_32}]},1).to({state:[{t:this.instance_33}]},1).to({state:[{t:this.instance_34}]},2).to({state:[{t:this.instance_35}]},1).to({state:[{t:this.instance_36}]},1).to({state:[{t:this.instance_37}]},1).to({state:[{t:this.instance_38}]},1).to({state:[{t:this.instance_39}]},1).to({state:[{t:this.instance_40}]},1).to({state:[{t:this.instance_41}]},1).to({state:[{t:this.instance_42}]},1).to({state:[{t:this.instance_43}]},2).to({state:[{t:this.instance_44}]},1).to({state:[{t:this.instance_45}]},1).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,500,624);
+
+
+// stage content:
+(lib.b = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Calque_1
+	this.instance = new lib.Symbole1();
+	this.instance.parent = this;
+	this.instance.setTransform(249.2,303.1,0.96,0.96,0,0,0,250.1,312);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(256.6,306.1,232.5,296.6);
+// library properties:
+lib.properties = {
+	id: 'CA6D6E71620A214B8FF4CD96AF6FA3EF',
+	width: 495,
+	height: 605,
+	fps: 12,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"banananantes_atlas_.jpg", id:"banananantes_atlas_"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['CA6D6E71620A214B8FF4CD96AF6FA3EF'] = {
+	getStage: function() { return exportRoot.getStage(); },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {		
+	var lastW, lastH, lastS=1;		
+	window.addEventListener('resize', resizeCanvas);		
+	resizeCanvas();		
+	function resizeCanvas() {			
+		var w = lib.properties.width, h = lib.properties.height;			
+		var iw = window.innerWidth, ih=window.innerHeight;			
+		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
+		if(isResp) {                
+			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
+				sRatio = lastS;                
+			}				
+			else if(!isScale) {					
+				if(iw<w || ih<h)						
+					sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==1) {					
+				sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==2) {					
+				sRatio = Math.max(xRatio, yRatio);				
+			}			
+		}			
+		domContainers[0].width = w * pRatio * sRatio;			
+		domContainers[0].height = h * pRatio * sRatio;			
+		domContainers.forEach(function(container) {				
+			container.style.width = w * sRatio + 'px';				
+			container.style.height = h * sRatio + 'px';			
+		});			
+		stage.scaleX = pRatio*sRatio;			
+		stage.scaleY = pRatio*sRatio;			
+		lastW = iw; lastH = ih; lastS = sRatio;            
+		stage.tickOnUpdate = false;            
+		stage.update();            
+		stage.tickOnUpdate = true;		
+	}
+}
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
