334ffb8f1e36b4b6ca5ff81669f967a3f3260342
Garnier Pierre-Henri <39243998+pierkiroule@users.noreply.github.com>
2022-12-23 11:59:01 +0100

    Update script.js

--- Com/script.js
+++ Com/script.js
@@ -1,23 +1,17 @@
-$(document).ready(function() {
-  // Chargement du fichier XML contenant la liste des défis
-  $.ajax({
-    type: "GET",
-    url: "challenges.xml",
-    dataType: "xml",
-    success: function(xml) {
-      // Fonction appelée lorsque le chargement du fichier XML a réussi
-      var challenges = $(xml).find("challenge"); // Récupération de tous les défis dans le fichier XML
-      var totalChallenges = challenges.length; // Nombre total de défis dans le fichier XML
-      
-      // Gestion du clic sur le bouton "Lancer un défi"
-      $("#launch-challenge").click(function() {
-        // Tirage aléatoire d'un défi parmi la liste de tous les défis
-        var randomIndex = Math.floor(Math.random() * totalChallenges);
-        var challenge = $(challenges[randomIndex]).text(); // Récupération du texte du défi sélectionné
-        
-        // Affichage du défi sélectionné dans l'interface web
-        $("#challenge-text").text(challenge);
-      });
-    }
-  });
-});
+ $(document).ready(function() {
+  // Liste des épreuves
+  const challenges = [
+    "Mimez le mot 'empathie'",
+    "Chantez le mot 'communication'",
+    "Dansez le mot 'bienveillance'",
+    "Jouez le mot 'bien-être'",
+    "Mimez le mot 'réassurance'",
+    "Chantez le mot 'soutien'",
+    "Dansez le mot 'écoute'",
+    "Jouez le mot 'acceptation'",
+    "Mimez le mot 'confiance'",
+    "Chantez le mot 'validation'"
+  ];
+
+  // Fonction de génération aléatoire d'un défi
+  function getRandomChallenge
