8a0d401aeeda33b16c453f797da566fbd2a0bac7
Garnier Pierre-Henri <39243998+pierkiroule@users.noreply.github.com>
2022-12-23 12:16:27 +0100

    Update script.js

--- Com/script.js
+++ Com/script.js
@@ -1,21 +1,9 @@
 $(document).ready(function() {
-  // Récupération de la liste des épreuves au format XML
-  $.get("challenges.xml", function(xml) {
-    // Fonction de callback qui sera exécutée une fois la liste des épreuves récupérée
-
-    // Sélection aléatoire d'un défi dans la liste
-    var randomIndex = Math.floor(Math.random() * $(xml).find("challenge").length);
-    var challenge = $(xml).find("challenge").eq(randomIndex).text();
-
-    // Affichage du défi sélectionné dans le formulaire
-    $("#challenge-form").text(challenge);
-
-    // Gestion du click sur le bouton "Lancer un défi"
-    $("#launch-challenge-button").click(function() {
-      // Sélection d'un nouveau défi aléatoire et mise à jour de l'affichage
-      randomIndex = Math.floor(Math.random() * $(xml).find("challenge").length);
-      challenge = $(xml).find("challenge").eq(randomIndex).text();
-      $("#challenge-form").text(challenge);
+    $.get('challenges.xml', function(data) {
+        var challenges = $(data).find('challenge');
+        $('#generate-challenge').click(function() {
+            var challenge = challenges.eq(Math.floor(Math.random() * challenges.length));
+            $('#challenge-container').html(challenge.text());
+        });
     });
-  });
 });
